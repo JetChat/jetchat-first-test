f8c16e92932c7acdacae74d5c065e4507563eb39
Ayfri <pierre.ayfri@gmail.com>
2021-09-02 18:21:32 +0200

    feat: Add DMChannel & ITextChannel, add some converters.

--- src/main/kotlin/Channel.kt
+++ src/main/kotlin/Channel.kt
@@ -1,17 +1,44 @@
+
 import kotlinx.datetime.Clock
 
 abstract class Channel(val type: ChannelType) {
 	abstract val id: Snowflake
+	abstract val isTextChannel: Boolean
+	
+	val asGuildChannel get() = this as GuildChannel
+	val asGuildChannelOrNull get() = this as? GuildChannel
+	val asGuildTextChannel get() = this as GuildTextChannel
+	val asGuildTextChannelOrNull get() = this as? GuildTextChannel
+	val asTextChannel get() = this as ITextChannel
+	val asTextChannelOrNull get() = this as? ITextChannel
 }
 
-class GuildTextChannel(name: String) : GuildChannel(name, ChannelType.GuildTextChannel) {
-	val messages = mutableMapOf<Snowflake, Message>()
+interface ITextChannel {
+	val id: Snowflake
+	val type: ChannelType
+	val messages: MutableMap<Snowflake, Message>
+	val isInGuild: Boolean
+	val isTextChannel get() = true
 }
 
-open class GuildChannel(val name: String, type: ChannelType) : Channel(type) {
+class DMChannel(val members: Pair<User, User>) : Channel(ChannelType.DMChannel), ITextChannel {
+	override val id: Snowflake = Clock.System.now().toEpochMilliseconds()
+	override val isInGuild = false
+	override val isTextChannel = true
+	override val messages = mutableMapOf<Snowflake, Message>()
+}
+
+class GuildTextChannel(name: String) : GuildChannel(name, ChannelType.GuildTextChannel), ITextChannel {
+	override val isInGuild = true
+	override val isTextChannel = true
+	override val messages = mutableMapOf<Snowflake, Message>()
+}
+
+abstract class GuildChannel(val name: String, type: ChannelType) : Channel(type) {
 	override val id: Snowflake = Clock.System.now().toEpochMilliseconds()
 }
 
 enum class ChannelType {
+	DMChannel,
 	GuildTextChannel
 }
