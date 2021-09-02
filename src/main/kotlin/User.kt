7e6c6285925e6d8e792553e526a6074a24e58781
Ayfri <pierre.ayfri@gmail.com>
2021-09-02 15:14:54 +0200

    feat: Add avatar property in User class, add defaultAvatar constant.

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,7 +1,16 @@
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.ui.graphics.ImageBitmap
 import kotlinx.datetime.Clock
 
 class User(var tag: String) {
 	val username get() = tag.dropLast(5)
 	val avatarUrl: String? = null
+	val avatar: ImageBitmap? get() = if(avatarUrl != null) loadNetworkImage(avatarUrl) else null
 	val id: Snowflake = Clock.System.now().toEpochMilliseconds()
+	
+	companion object {
+		val defaultAvatar = Icons.Outlined.Person
+	}
 }
