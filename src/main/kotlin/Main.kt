e4332d9b07ab59d7e3945fdae13680551194eb00
Ayfri <pierre.ayfri@gmail.com>
2021-09-14 10:23:13 +0200

    tests: Test guild.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,10 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import composables.TextChannel
-import entities.DMChannel
+import composables.Guild
+import entities.Guild
+import entities.GuildMember
+import entities.GuildTextChannel
 import entities.Message
 import entities.User
 
@@ -17,17 +19,25 @@ import entities.User
 @Preview
 fun App() {
 	val text = "Hello, World!"
-	val user by remember { mutableStateOf(User("Ayfri#0000")) }
-	val user2 by remember { mutableStateOf(User("Ayfri#0001")) }
-	val channel by remember { mutableStateOf(DMChannel(user to user2)) }
-	var message: Message
+	val guild by remember { mutableStateOf(Guild("test")) }
+	val channel = GuildTextChannel("test", guild)
+	guild.channels[channel.id] = channel
 	
-	for (i in 0..8000) {
+	val user = User("Ayfri#0000")
+	val user2 = User("Ayfri#0001")
+	guild.members[user.id] = GuildMember(guild, user)
+	guild.members[user2.id] = GuildMember(guild, user2)
+	
+	var message: Message
+	for (i in 0..5) {
 		message = Message("$text $i", user)
 		channel.messages[message.id] = message
 	}
 	
-	TextChannel(channel)
+	Guild(guild)
+	
+	
+//	TextChannel(channel)
 }
 
 fun main() = application {
