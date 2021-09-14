00efc04eefa065063f01978d0cb342eb1f1455ca
Ayfri <pierre.ayfri@gmail.com>
2021-09-14 09:25:59 +0200

    feat: Add SendBox composable.

--- src/main/kotlin/composables/Channel.kt
+++ src/main/kotlin/composables/Channel.kt
@@ -10,12 +10,22 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,20 +50,51 @@ fun TextChannel(channel: ITextChannel) {
 			TextChannelButtons(channel)
 		}
 		Row(
-			horizontalArrangement = Arrangement.SpaceBetween
+			horizontalArrangement = Arrangement.SpaceBetween,
+			modifier = Modifier.weight(1f)
 		) {
 			MessageList(channel)
 			ChannelMembers(channel)
+		}
+		SendBox()
+	}
+}
+
+@Composable
+fun SendBox(modifier: Modifier = Modifier) {
+	Row(
+		modifier = Modifier.background(Color(240, 240, 240)).fillMaxWidth().padding(10.dp).then(modifier)
+	) {
+		var text by remember { mutableStateOf("") }
+		OutlinedTextField(
+			text,
+			modifier = Modifier.fillMaxWidth(0.8f).requiredHeightIn(50.dp, 400.dp),
+			onValueChange = {
+				text = it
+			},
+			label = {
+				Text("")
+			},
+			placeholder = {
+				Text("Enter Text")
+			}
+		)
+		IconButton(
+			modifier = Modifier.align(Alignment.CenterVertically),
+			onClick = {
+			}
+		) {
+			Icon(Icons.Default.Send, "send")
 		}
 	}
 }
 
 @Composable
-fun MessageList(channel: ITextChannel) {
+fun MessageList(channel: ITextChannel, modifier: Modifier = Modifier) {
 	val messages = channel.messages.values.toList().sortedBy { it.id }
 	
 	Box(
-		modifier = Modifier.fillMaxWidth(0.9f)
+		modifier = Modifier.fillMaxWidth(0.9f).then(modifier)
 	) {
 		val listState = rememberLazyListState()
 		
