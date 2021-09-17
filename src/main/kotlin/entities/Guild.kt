98b3af87b18585c27dca38ac84c5c9570e76bc66
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 17:12:01 +0200

    feat: Add add & delete channel methods to Guild class.

--- src/main/kotlin/entities/Guild.kt
+++ src/main/kotlin/entities/Guild.kt
@@ -8,4 +8,15 @@ class Guild(val name: String) {
 	val createdAt = Clock.System.now()
 	val id: Snowflake = createdAt.toEpochMilliseconds()
 	val members = mutableMapOf<Snowflake, GuildMember>()
+	
+	fun createChannel(name: String, type: ChannelType): GuildChannel? {
+		val channel = object : GuildChannel(name, type, this) {
+			override val isTextChannel = type.isTextType
+		}
+		return channels.put(channel.id, channel)
+	}
+	
+	fun deleteChannel(name: Snowflake): GuildChannel? {
+		return channels.remove(name)
+	}
 }
