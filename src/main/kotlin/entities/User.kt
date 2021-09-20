3e023510406523c1e0a23c0527534f321341538b
Ayfri <pierre.ayfri@gmail.com>
2021-09-20 11:10:51 +0200

    feat: Use globalId for Users.

--- src/main/kotlin/entities/User.kt
+++ src/main/kotlin/entities/User.kt
@@ -7,14 +7,27 @@ import androidx.compose.ui.graphics.ImageBitmap
 import kotlinx.datetime.Clock
 import loadNetworkImage
 
+var lastUserCreatedInstant = Clock.System.now()
+
 class User(var tag: String) {
 	val avatarUrl: String? = null
 	val avatar: ImageBitmap? get() = if (avatarUrl != null) loadNetworkImage(avatarUrl) else null
 	val createdAt = Clock.System.now()
-	val id: Snowflake = createdAt.toEpochMilliseconds()
+	val id: Snowflake
 	val username get() = tag.dropLast(5)
 	
 	companion object {
 		val defaultAvatar = Icons.Outlined.Person
+	}
+	
+	init {
+		if (lastUserCreatedInstant == createdAt) {
+			globalId++
+		} else {
+			lastUserCreatedInstant = createdAt
+			globalId = 0
+		}
+		
+		id = "${createdAt.toEpochMilliseconds()}${globalId.toString().padStart(3, '0')}".toLong()
 	}
 }
