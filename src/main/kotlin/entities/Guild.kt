9b439575ae049f9fde82572f33c62c30ed0f0e58
Ayfri <pierre.ayfri@gmail.com>
2021-09-20 11:11:52 +0200

    feat: Add methods for adding/removing users from guild, imporve createChannel.

--- src/main/kotlin/entities/Guild.kt
+++ src/main/kotlin/entities/Guild.kt
@@ -5,15 +5,40 @@ import kotlinx.datetime.Clock
 
 class Guild(val name: String) {
 	val channels = mutableMapOf<Snowflake, GuildChannel>()
+	val textChannels get() = channels.values.filterIsInstance<GuildTextChannel>()
 	val createdAt = Clock.System.now()
 	val id: Snowflake = createdAt.toEpochMilliseconds()
 	val members = mutableMapOf<Snowflake, GuildMember>()
 	
-	fun createChannel(name: String, type: ChannelType): GuildChannel? {
-		val channel = object : GuildChannel(name, type, this) {
-			override val isTextChannel = type.isTextType
+	fun addMember(member: GuildMember): GuildMember {
+		members[member.id] = member
+		return member
+	}
+	
+	fun addMember(member: User): GuildMember {
+		val guildMember = GuildMember(this, member)
+		members[member.id] = guildMember
+		return guildMember
+	}
+	
+	fun getMember(tag: String): GuildMember? {
+		return members.values.find { it.tag == tag }
+	}
+	
+	fun removeMember(id: Snowflake): GuildMember? {
+		return members.remove(id)
+	}
+	
+	fun createChannel(name: String, type: ChannelType): GuildChannel {
+		val channel = when (type) {
+			ChannelType.GuildTextChannel -> GuildTextChannel(name, this)
+			else -> object : GuildChannel(name, type, this) {
+				override val isTextChannel = type.isTextType
+			}
 		}
-		return channels.put(channel.id, channel)
+		
+		channels[channel.id] = channel
+		return channel
 	}
 	
 	fun deleteChannel(name: Snowflake): GuildChannel? {
